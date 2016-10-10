6945a3fe65815acbe93665eedcaf01ff104494e5
Jochen Scheib <webmaster@mapero.de>
2016-10-10 20:03:56 +0200

    rewrite with new lib

--- fritz.js
+++ fritz.js
@@ -1,79 +1,77 @@
-var tr064lib = require("tr-064"),
+var Fritzbox = require("fritzbox"),
 http = require("http"),
 xml2js = require("xml2js");
 
-var client = new tr064lib.TR064();
+var Promise = require("bluebird");
+var request = require("request");
+
 var parser = xml2js.Parser({explicitRoot: false, explicitArray: false});
 
 module.exports = function(RED) {
 
 	RED.httpAdmin.get('/fritzbox/services', function(req, res, next) {
-		var provider = req.query.provider;
 		var url = req.query.url;
-		switch(provider) {
-			case "IGD":
-				client.initIGDDevice(req.query.url, 49000, function (err, device) {
-					if (!err) {
-						var services = Object.keys(device.services);
-						res.end(JSON.stringify(services));
-					}
-				});
-				break;
-			case "TR064":
-				client.initTR064Device(req.query.url, 49000, function (err, device) {
-					if (!err) {
-						var services = Object.keys(device.services);
-						res.end(JSON.stringify(services));
-					}
-				});
-				break;
-			default:
+		var config = {
+			host: req.query.url
+		};
+
+		var fritzbox = new Fritzbox.Fritzbox(config);
+
+		Promise.all([fritzbox.initIGDDevice(), fritzbox.initTR064Device()])
+			.then(function() {
+				var services = Object.keys(fritzbox.services);
+				res.end(JSON.stringify(services));
+			}).catch(function(error) {
 				res.end();
-		}
+			});
 	});
 
 	RED.httpAdmin.get('/fritzbox/actions', function(req, res, next) {
-		var url = req.query.url;
-		var provider = req.query.provider;
 		var service = req.query.service;
 
-		switch(provider) {
-			case "IGD":
-				client.initIGDDevice(req.query.url, 49000, function (err, device) {
-					if(!err && device.services[service]) {
-						var actions = device.services[service].meta.actionsInfo;
-						res.end(JSON.stringify(actions));
-					} else {
-						res.end(JSON.stringify([]));
-					}
-				});
-				break;
-			case "TR064":
-				client.initTR064Device(req.query.url, 49000, function (err, device) {
-					if(!err && device.services[service]) {
-						var actions = device.services[service].meta.actionsInfo;
-						res.end(JSON.stringify(actions));
-					} else {
-						res.end(JSON.stringify([]));
-					}
-				});
-				break;
-			default:
+		var config = {
+			host: req.query.url
+		};
+
+		var fritzbox = new Fritzbox.Fritzbox(config);
+
+		Promise.all([fritzbox.initIGDDevice(), fritzbox.initTR064Device()])
+			.then(function() {
+				var actions = fritzbox.services[service].actionsInfo;
+				res.end(JSON.stringify(actions));
+			}).catch(function(error) {
 				res.end();
-			}
+			});
 	});
 
 	function FritzboxConfig(n) {
 		RED.nodes.createNode(this, n);
 		var node = this;
 		node.host = n.host;
-		node.providers = {};
 		node.timer = null;
 		node.state = null;
 
 		if(!node.host) return;
 
-		function updateStatus(status) {
+		var config = {
+			host: n.host,
+			user: node.credentials.username,
+			password: node.credentials.password
+		};
+
+		node.fritzbox = new Fritzbox.Fritzbox(config);
+
+		node.reinit = function() {
+			Promise.all([node.fritzbox.initIGDDevice(), node.fritzbox.initTR064Device()])
+				.then(function() {
+					updateStatus("ready");
+				}).catch(function(error) {
+					node.error("Initialization of device failed. Check configuration. Error: "+error);
+					updateStatus("error");
+				});
+		};
+
+		var updateStatus = function(status) {
 			node.state = status;
 			switch(status) {
 				case "init":
@@ -87,35 +85,6 @@ module.exports = function(RED) {
 					break;
 			}
 		}
-
-		node.reinit = function() {
-			if(node.state !== "init") {
-				updateStatus("init");
-				node.log("Initializing tr064 ...");
-				client.initTR064Device(node.host, 49000, function(err, tr064) {
-					if(err || !tr064) {
-						node.error("Initialization of device failed. Check configuration. Error: "+err);
-						updateStatus("error");
-					} else {
-						node.log("Successfully initialzed tr064 device.");
-						node.providers.TR064 = tr064;
-						node.providers.TR064.httplogin(node.credentials.username, node.credentials.password);
-						client.initIGDDevice(node.host, 49000, function(err, igd) {
-							if(err || !igd) {
-								node.error("Initialization of device failed. Check configuration. Error: "+err);
-								updateStatus("error");
-							} else {
-								node.log("Successfully initialzed igd device.");
-								node.providers.IGD = igd;
-								node.providers.IGD.httplogin(node.credentials.username, node.credentials.password);
-								updateStatus("ready");
-							}
-						});
-					}
-				});
-			}
-		};
-
 		node.reinit();
 	}
 	RED.nodes.registerType("fritzbox-config", FritzboxConfig, {
@@ -129,7 +98,6 @@ module.exports = function(RED) {
 	function FritzboxIn(n) {
 		RED.nodes.createNode(this,n);
 		var node = this;
-		node.provider = n.provider;
 		node.service = n.service;
 		node.action = n.action;
 		node.config = RED.nodes.getNode(n.device);
@@ -137,20 +105,17 @@ module.exports = function(RED) {
 		node.config.on('statusUpdate', node.status);
 
 		node.on('input', function(msg) {
-			if(node.config.state === "ready" && node.provider) {
-				var provider = msg.provider ? msg.provider : node.provider;
+			if(node.config.state === "ready" && node.config.fritzbox) {
 				var service = msg.service ? msg.service : node.service;
 				var action = msg.action ? msg.action : node.action;
 
-				node.config.providers[provider].services[service].actions[action](msg.payload, function(err, result) {
-					if(err) {
-						node.warn(err);
-						return;
-					} else {
+				node.config.fritzbox.services[service].actions[action](msg.payload)
+					.then(function(result) {
 						msg.payload = result;
 						node.send(msg);
-					}
-				});
+					}).catch(function(error) {
+						node.error(error);
+					});
 			} else {
 				node.error("Device not ready.");
 				node.config.reinit();
@@ -172,34 +137,22 @@ module.exports = function(RED) {
 		node.config.on('statusUpdate', node.status);
 
 		node.on('input', function(msg) {
-			if(node.config.state === "ready" && node.config.providers.TR064) {
-				node.config.providers.TR064.services["urn:dslforum-org:service:X_AVM-DE_OnTel:1"].actions.GetCallList(function(err, result) {
-					if(err) {
-						node.warn(err);
-						return;
-					} else {
+			if(node.config.state === "ready" && node.config.fritzbox) {
+				node.config.fritzbox.services["urn:dslforum-org:service:X_AVM-DE_OnTel:1"].actions.GetCallList()
+					.then(function(url) {
 						if(n.max) {
-							result.NewCallListURL += "&max=" + n.max;
+							url.NewCallListURL += "&max=" + n.max;
 						}
-						http.get(result.NewCallListURL, function(result) {
-							var data = "";
-							result.on('data', function(chunk) {
-								data += chunk;
-							});
-							result.on('end', function() {
-								parser.parseString(data, function(err, result) {
-									if(err) {
-										node.error(err);
-										return;
-									} else {
-										msg.payload = result;
-										node.send(msg);
-									}
-								});
-							});
-						});
-					}
-				});
+						return Promise.promisify(request.get, {multiArgs: true})(url.NewCallListURL);
+					}).then(function(result) {
+						var body = result[1];
+						return Promise.promisify(parser.parseString)(body);
+					}).then(function(result) {
+						msg.payload = result;
+						node.send(msg);
+					}).catch(function(error) {
+						node.error(error);
+					});
 			} else {
 				node.error("Device not ready.");
 				node.config.reinit();
